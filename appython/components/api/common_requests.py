5881b175a3cafd9f0cc08ffaff4c2075bb7ae1b3
Antonio Zugaldia <antonio@zugaldia.net>
2015-04-07 09:47:07 -0400

    add true values

--- appython/components/api/common_requests.py
+++ appython/components/api/common_requests.py
@@ -1,5 +1,23 @@
 from flask.ext.restful import reqparse
 
+'''
+For boolean requests, when we define the type as bool (like
+bool_request below), the value is always true as long as it's set,
+even if we set it to "false". For that reason, it's probably easier to
+use a string_request and check if the value is in this list:
+
+    flag = args.get('flag') in TRUE_VALUES
+
+We also include the unicode character for the check mark (cool, right?)
+http://en.wikipedia.org/wiki/Check_mark#Unicode
+'''
+
+TRUE_VALUES = ['true', 'True', 'TRUE', '1', u'\u2713']
+
+'''
+Requests
+'''
+
 string_request = reqparse.RequestParser()
 string_request.add_argument('text', type=unicode)
 
